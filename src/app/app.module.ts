1873e2bc7b18671d2be35e1660505a41fc8b4bc3
Diego Emilio Fuentes Gomez <fuentesdiego054@gmail.com>
2019-02-09 17:55:25 -0300

    componentes y rutas e imagenes planetas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {  APP_ROUTING} from './app.routes';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SomosComponent } from './components/somos/somos.component';
+import { PortafolioComponent } from './components/portafolio/portafolio.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FooterComponent,
+    SomosComponent,
+    PortafolioComponent,
+    ContactoComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    APP_ROUTING
   ],
   providers: [],
   bootstrap: [AppComponent]
